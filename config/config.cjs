14e0e946d1456c19a6facdef6a07a1cd6424c9d2
Rahma Rubi <rahmaelroby47@gmail.com>
2026-04-17 14:00:18 +0200

    switched to cockroach

--- config/config.cjs
+++ config/config.cjs
@@ -3,10 +3,22 @@ require('dotenv').config();
 module.exports = {
   development: {
     use_env_variable: "DB_URL",
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
     use_env_variable: "DB_URL",
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };

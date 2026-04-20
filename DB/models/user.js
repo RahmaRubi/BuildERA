08ad9dd7b5e8538a9d5f4f2bfebfac72f91cc012
Rahma Rubi <rahmaelroby47@gmail.com>
2026-04-20 19:22:00 +0200

    user schema update

--- DB/models/user.js
+++ DB/models/user.js
@@ -1,10 +1,14 @@
 export default (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-        name: { type: DataTypes.STRING, allowNull: false },
+        userName: { type: DataTypes.STRING, allowNull: false },
         email: { type: DataTypes.STRING, allowNull: false, unique: true },
         password: { type: DataTypes.STRING, allowNull: false },
-        role: { type: DataTypes.STRING, defaultValue: 'user' }
-    })
+        phone: { type: DataTypes.STRING },
+        role: { type: DataTypes.STRING, defaultValue: 'user' },
+        isConfirmed: { type: DataTypes.BOOLEAN, defaultValue: false },
+        isDeleted: { type: DataTypes.BOOLEAN, defaultValue: false },
+        deletedAt: { type: DataTypes.DATE, allowNull: true },
+    }, { paranoid: false })
     User.associate = (db) => {
         User.hasMany(db.Build, { foreignKey: { name: 'user_id', allowNull: false }, onDelete: 'CASCADE' })
     }
